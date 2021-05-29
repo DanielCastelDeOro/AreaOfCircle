ec0dcc6f78acde9eca31871836cd052aa6794e77
Daniel Castel De Oro <daniel.casteldeoro@gmail.com>
2021-05-29 05:06:19 -0600

    initial commit

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        public static double CalculateArea(double radius);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the area of a circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            var areaOfCircle = CalculateArea(radius);
+            
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+
+            return Math.PI * (radius * radius);
+        }
+        
+            
+        
+    }
+}
