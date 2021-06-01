dc68dc7c6c6a833604f207e9948dd3b8fcde0b55
Jeremy Huddleston <jshudd@gmail.com>
2021-06-01 16:50:45 -0500

    added comments
    added comments to project to explain changes

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -4,13 +4,17 @@ namespace AreaOfCircle
 {
     class Program
     {
+        //Methods need a body enclosed in {}
+        //That's where the work is done/math is calculated/magic happens
         public static double CalculateArea(double radius)
         {
+            //moved below line from Main method into this CalculateArea method so it can calculate area
             return Math.PI * (radius * radius);
         }
 
         static void Main(string[] args)
         {
+            //Changed question below to ask for radius from user
             Console.WriteLine("What is the radius of the circle?");
 
             var radius = double.Parse(Console.ReadLine());
